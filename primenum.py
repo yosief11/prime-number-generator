4e5ec9e3836579c24d2df470b4f5bd57658501a2
Yosief Teckle <109878966+yosief11@users.noreply.github.com>
2023-01-10 21:25:28 -0800

    Update primenum.py

--- primenum.py
+++ primenum.py
@@ -1,1 +1,25 @@
+from math import sqrt
 
+def prime (num) :
+    prime = True
+    for i in range(2, round(sqrt(num) + 1 )):
+        if num % i == 0:
+            prime = False
+            break
+        return prime
+
+def main():
+    print ("This program prints all prime numbers less than ")
+    print(" or equal to a number")
+    num = eval(input ("Enter a number: "))
+
+    print ("These are prime numbers less than or equal to "+ str(
+num) + ":")
+    for i in range(2, num + 1):
+        if prime(i):
+            if i == 2:
+                print(2, end = "")
+        else:
+            print(",", i, end = "")
+    print()
+main()
